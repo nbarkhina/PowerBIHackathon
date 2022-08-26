af5135deb1cebde08229bfc7e61a23a167e553e0
Neil Barkhina Lenovo <nbarkhina@gmail.com>
2022-08-26 14:53:28 -0400

    initial commit

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var neilVisualBE5DFFFDED03470EAD4DF8EA0BF479AC_DEBUG: IVisualPlugin;
+export default neilVisualBE5DFFFDED03470EAD4DF8EA0BF479AC_DEBUG;
